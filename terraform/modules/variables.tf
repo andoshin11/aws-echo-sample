37d68dfdedf718217cec466939457b836d73e685
andoshin11 <shinglish11@gmail.com>
2019-10-31 11:57:19 +0900

    Add module common variables

---
+++ terraform/modules/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  default = "echo-sample"
+}
+
+variable "aws_region" {}
+
+variable "bucket" {
+  default = "echo-sample"
+}
+
+variable "alb_certificate_arn" {
+  default = "arn:aws:acm:ap-northeast-1:xxxxx:certificate/zzzzz1"
+}
+
